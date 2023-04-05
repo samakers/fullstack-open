cb35375135d8d40f69d8ba5c93c8e0d4e5afcfa3
Sam Akers <sam.akers1@gmail.com>
2023-04-05 18:23:15 +0100

    2.18 completed - return more info

--- part2/countries/countries/src/App.js
+++ part2/countries/countries/src/App.js
@@ -25,21 +25,40 @@ function App() {
   return (
     <div>
       <Filter value={filter} onChange={handleFilterChange} />
-      {filter && (
+      {filter.length > 0 ? (
         <div>
-          {filteredCountries.length > 10 ? (
-            <p>Too many matches, specify another filter</p>
+          {filteredCountries.length === 1 ? (
+            <div>
+              <h2>{filteredCountries[0].name.common}</h2>
+              <p>Capital: {filteredCountries[0].capital}</p>
+              <p>Population: {filteredCountries[0].population}</p>
+              <h3>Languages:</h3>
+              <ul>
+                {Object.values(filteredCountries[0].languages).map((language, index) => (
+                  <li key={index}>{language}</li>
+                ))}
+              </ul>
+              <img src={filteredCountries[0].flags.png} alt={`${filteredCountries[0].name.common} flag`} width="200" />
+            </div>
           ) : (
-            <ul>
-              {filteredCountries.map((country, index) => (
-                <li key={index}>{country.name.common}</li>
-              ))}
-            </ul>
+            filteredCountries.length > 10 ? (
+              <p>Too many matches, specify another filter</p>
+            ) : (
+              <ul>
+                {filteredCountries.map((country, index) => (
+                  <li key={index}>{country.name.common}</li>
+                ))}
+              </ul>
+            )
           )}
         </div>
+      ) : (
+        <p>Type a filter to search for countries</p>
       )}
     </div>
   );
+  
+  
 }
 
 export default App;
