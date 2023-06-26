4e0e33855eb1d24a13eb0a62af9e524923edb05e
samakers <41086623+samakers@users.noreply.github.com>
2023-06-26 21:00:10 +0100

    Delete part2-notes-part2-1 directory

--- part2-notes-part2-1/src/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-
-axios.get('http://localhost:3001/notes').then(response => {
-  const notes = response.data
-  ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
-})
